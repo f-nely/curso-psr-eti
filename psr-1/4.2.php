1c6a67d0671ac702c122c7bfdc78b1f6134a7039
nely de faria <nelly.nf54@gmail.com>
2021-11-12 11:31:57 -0300

    Criando arquivo 4.2.php

---
+++ psr-1/4.2.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This guide intentionally avoids any recommendation regarding the use of $StudlyCaps, $camelCase, or $under_score property names.
+ */
+
+class Query
+{
+  private $NameVar, $nameVar, $name_var;
+}
