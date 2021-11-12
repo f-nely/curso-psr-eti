1d04ff4a095f1e3f4edadd24c4f053360b2d1b17
nely de faria <nelly.nf54@gmail.com>
2021-11-12 11:32:31 -0300

    Criando arquivo 4.3.php

---
+++ psr-1/4.3.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Method names MUST be declared in camelCase().
+ */
+
+class Query
+{
+  public function nameMethod()
+  {
+
+  }
+
+  public function otherNameMethod($prm1, $prm2)
+  {
+
+  }
+}
