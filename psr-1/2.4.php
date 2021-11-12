cb53f1edb691657f7daab2fa48ddd52dc70d4a30
nely de faria <nelly.nf54@gmail.com>
2021-11-12 11:30:46 -0300

    Criando arquivo 2.4.php

---
+++ psr-1/2.4.php
@@ -0,0 +1,15 @@
+<?php
+
+// declaration
+function foo()
+{
+    // function body
+}
+
+// conditional declaration is *not* a side effect
+if (! function_exists('bar')) {
+    function bar()
+    {
+        // function body
+    }
+}
